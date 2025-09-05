e16fc8b989f9aa9740c1130386827aba779d835f
Kamil Jan Mularski <kammul@outlook.com>
2025-09-06 00:26:46 +0200

    Stupid test of name convention

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,12 +13,12 @@ public class WorkWithFile {
     public static final int RECORD_SIZE = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[][] csv = readCSV(fromFileName);
+        String[][] csv = readCsv(fromFileName);
         Report report = Report.make(csv);
-        saveToCSV(report, toFileName);
+        saveToCsv(report, toFileName);
     }
 
-    public String[][] readCSV(String fileName) {
+    public String[][] readCsv(String fileName) {
         try {
             List<String> allLines = Files.readAllLines(Path.of(fileName));
             String[][] data = new String[allLines.size()][RECORD_SIZE];
@@ -34,7 +34,7 @@ public class WorkWithFile {
         }
     }
 
-    public void saveToCSV(Report report, String fileName) {
+    public void saveToCsv(Report report, String fileName) {
         String[] lines = new String[] {
                 "supply," + report.getSupply(),
                 "buy," + report.getBuy(),
