dce8868159b20703db65075d2a6d53ad6b834267
Kamil Jan Mularski <kammul@outlook.com>
2025-09-06 00:22:27 +0200

    First try

---
+++ src/main/java/core/basesyntax/Report.java
@@ -0,0 +1,46 @@
+package core.basesyntax;
+
+public class Report {
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
+    private static final int VALUES_COUNT = 2;
+    private static final String SUPPLY_OPERATION = "supply";
+    private static final String BUY_OPERATION = "buy";
+    private final int supply;
+    private final int buy;
+
+    private Report(int supply, int buy) {
+        this.supply = supply;
+        this.buy = buy;
+    }
+
+    public int getSupply() {
+        return supply;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    private static Report make(int[][] data) {
+        int supply = 0;
+        int buy = 0;
+        for (int[] record : data) {
+            supply += record[SUPPLY_INDEX];
+            buy += record[BUY_INDEX];
+        }
+        return new Report(supply, buy);
+    }
+
+    public static Report make(String[][] data) {
+        int[][] values = new int[data.length][VALUES_COUNT];
+        int i = 0;
+        for (String[] record : data) {
+            String operation = record[WorkWithFile.OPERATION_TYPE_INDEX];
+            int amount = Integer.parseInt(record[WorkWithFile.AMOUNT_INDEX]);
+            values[i][SUPPLY_INDEX] = operation.equals(SUPPLY_OPERATION) ? amount : 0;
+            values[i++][BUY_INDEX] = operation.equals(BUY_OPERATION) ? amount : 0;
+        }
+        return make(values);
+    }
+}
--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,49 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 
+public class WorkWithFile {
+
+    public static final int OPERATION_TYPE_INDEX = 0;
+    public static final int AMOUNT_INDEX = 1;
+    public static final int RECORD_SIZE = 2;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String[][] csv = readCSV(fromFileName);
+        Report report = Report.make(csv);
+        saveToCSV(report, toFileName);
+    }
+
+    public String[][] readCSV(String fileName) {
+        try {
+            List<String> allLines = Files.readAllLines(Path.of(fileName));
+            String[][] data = new String[allLines.size()][RECORD_SIZE];
+            int i = OPERATION_TYPE_INDEX;
+            for (String line : allLines) {
+                String[] record = line.split(",");
+                data[i][OPERATION_TYPE_INDEX] = record[OPERATION_TYPE_INDEX];
+                data[i++][AMOUNT_INDEX] = record[AMOUNT_INDEX];
+            }
+            return data;
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read the file", e);
+        }
+    }
+
+    public void saveToCSV(Report report, String fileName) {
+        String[] lines = new String[] {
+                "supply," + report.getSupply(),
+                "buy," + report.getBuy(),
+                "result," + (report.getSupply() - report.getBuy())
+        };
+        try {
+            Files.write(Path.of(fileName), Arrays.asList(lines));
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot write the file", e);
+        }
     }
 }
